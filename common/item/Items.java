55e3c0b14fadc0068e471e8d2c8dac15d29aea69
J. Wostenberg <awosty@gmail.com>
2013-04-11 07:11:09 -0600

    Tweaking the tools some more

--- common/item/Items.java
+++ common/item/Items.java
@@ -18,13 +18,13 @@ public class Items {
 	
 	// Obsidian tools, with a harvest speed of 7.75 -- almost as fast as diamond (which is 8)
 	public static int ObsidianHarvestLevel = 3;
-	public static EnumToolMaterial ObsidianTools = EnumHelper.addToolMaterial("OBSIDIAN", ObsidianHarvestLevel, 1561, 8.0F, 3, 10);
+	public static EnumToolMaterial ObsidianTools = EnumHelper.addToolMaterial("OBSIDIAN", ObsidianHarvestLevel, 1561, 8.0F, 2, 10);
 	// Glass tools; basically halfway between iron and diamond but with a higher enchantability than both
 	public static int GlassHarvestLevel = 2;
 	public static EnumToolMaterial GlassTools = EnumHelper.addToolMaterial("GLASS", GlassHarvestLevel, 250, 7.5F, 2, 18);
 	// Ender tools
 	public static int EnderHarvestLevel = 3;
-	public static EnumToolMaterial EnderTools = EnumHelper.addToolMaterial("ENDER", EnderHarvestLevel, 1561, 8.0F, 3, 22);
+	public static EnumToolMaterial EnderTools = EnumHelper.addToolMaterial("ENDER", EnderHarvestLevel, 1561, 8.0F, 4, 22);
 	
 	public static Item shovelObsidian = new ItemSpade(1551, ObsidianTools).setIconIndex(0).setTextureFile(CommonProxy.TEXTURES_PNG).setItemName("obsidianShovel");
 	public static Item pickaxeObsidian = new ItemPickaxe(1552, ObsidianTools).setIconIndex(1).setTextureFile(CommonProxy.TEXTURES_PNG).setItemName("obsidianPickaxe");
