d859914fbe9dad7b3118e4fddf0121f2159c8786
J. Wostenberg <awosty@gmail.com>
2013-04-13 13:07:55 -0600

    Trinium processor tile entity now implements IIventory

--- common/tileentity/TileEntityTriniumProcessor.java
+++ common/tileentity/TileEntityTriniumProcessor.java
@@ -1,13 +1,75 @@
 package jw.taw.common.tileentity;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 
-public class TileEntityTriniumProcessor extends TileEntity {
+public class TileEntityTriniumProcessor extends TileEntity implements IInventory {
 	// The item stacks being used in the processor (fuel, water, input, and output)
 	private ItemStack[] processorItemStacks = new ItemStack[4];
 	// The number of ticks left until the machine finish processing the trinium
 	private int processingTime = 0;
 	
 	public TileEntityTriniumProcessor() {}
+
+	@Override
+	public int getSizeInventory() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int var1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ItemStack decrStackSize(int var1, int var2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int var1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInventorySlotContents(int var1, ItemStack var2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getInvName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getInventoryStackLimit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer var1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void openChest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeChest() {
+		// TODO Auto-generated method stub
+		
+	}
 }
